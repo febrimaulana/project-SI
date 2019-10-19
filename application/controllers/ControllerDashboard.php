2d1f9237df05f390c91fea202ab331b027a46d76
febrimaulana <febriyunus@gmail.com>
2019-10-20 00:43:18 +0700

    halaman dashboard dengan codeigniter

---
+++ application/controllers/ControllerDashboard.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class ControllerDashboard extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['title'] = "Dashboard";
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('dashboard/index', $data);
+        $this->load->view('templates/footer', $data);
+    }
+}
+
+/* End of file ControllerDashboard.php */
