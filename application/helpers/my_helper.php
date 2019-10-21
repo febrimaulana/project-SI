a07b508a32b4d96b9a545185e3237e0cee69d880
armansyahfauzi378 <armanfauzi1112@gmail.com>
2019-10-21 20:33:52 +0700

    template

--- application/helpers/my_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function templates($lokasi, $data)
-{
-	$ci = get_instance();
-	$ci->load->view("templates/header", $data);
-	$ci->load->view("templates/sidebar", $data);
-	$ci->load->view("$lokasi", $data);
-	$ci->load->view("templates/footer", $data);
-}
